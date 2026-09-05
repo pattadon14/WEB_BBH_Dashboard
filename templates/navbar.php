cdd475232b1ed15a4dc26eddd17ef0f5e874802b
pattadon14 <128819513+pattadon14@users.noreply.github.com>
2026-09-05 10:36:36 +0700

    Connect BBH navbar hamburger to slide-out sidebar

--- templates/navbar.php
+++ templates/navbar.php
@@ -2,42 +2,28 @@
 
     <!-- Left -->
     <ul class="navbar-nav">
-
         <li class="nav-item">
-
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button">
-
+            <button type="button" class="bbh-menu-toggle" id="bbh-menu-toggle"
+                aria-label="เปิดเมนู" aria-expanded="false" aria-controls="bbh-sidebar">
                 <i class="fas fa-bars"></i>
-
-            </a>
-
+            </button>
         </li>
-
     </ul>
 
     <!-- Title -->
     <a href="<?= BASE_URL ?>" class="navbar-brand fw-bold">
-
         BBH DASHBOARD (โรงพยาบาลบ้านบึง)
-
     </a>
 
     <!-- Right -->
     <ul class="navbar-nav ms-auto">
-
         <li class="nav-item">
-
             <a href="https://docs.google.com/forms/d/e/1FAIpQLScKGR0_xvvq1uHqdwm3Npj0y6tEAZq0o5KEp9tQaufokUWzwg/viewform?usp=dialog"
                 target="_blank" class="feedback-btn">
-
                 <i class="fas fa-comment-dots"></i>
-
                 <span>เสนอความคิดเห็น</span>
-
             </a>
-
         </li>
-
     </ul>
 
 </nav>
