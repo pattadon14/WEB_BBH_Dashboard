d63ef121eeb113c66dd04903920e6b3875ad651a
pattadon14 <128819513+pattadon14@users.noreply.github.com>
2026-09-07 09:54:59 +0700

    Load HOSxP login modal JavaScript

--- templates/footer.php
+++ templates/footer.php
@@ -6,6 +6,12 @@
 
 <script src="<?= BASE_URL ?>assets/AdminLTE/dist/js/adminlte.min.js"></script>
 
+<!-- BBH Sidebar -->
+<script src="<?= BASE_URL ?>assets/js/sidebar.js"></script>
+
+<!-- HOSxP Login Modal -->
+<script src="<?= BASE_URL ?>assets/js/login.js"></script>
+
 </body>
 
 </html>
